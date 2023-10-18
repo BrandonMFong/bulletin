b37b53ed64abbfbd4e03c7970e1ad0d25050991d
Brando <fong.m.brandon97@gmail.com>
2023-10-18 15:02:47 -0700

    Adding idf dependencies

--- firmware/main/main.c
+++ firmware/main/main.c
@@ -13,6 +13,42 @@
 #include "driver/gpio.h"
 #include "sdkconfig.h"
 #include "esp_log.h"
+#include "esp_timer.h"
+#include "esp_lcd_panel_io.h"
+#include "esp_lcd_panel_ops.h"
+#include "driver/i2c.h"
+#include "esp_err.h"
+#include "lvgl.h"
+#include "esp_lvgl_port.h"
+
+#if CONFIG_EXAMPLE_LCD_CONTROLLER_SH1107
+#include "esp_lcd_sh1107.h"
+#else
+#include "esp_lcd_panel_vendor.h"
+#endif
+
+#define I2C_HOST  0
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////// Please update the following configuration according to your LCD spec //////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#define EXAMPLE_LCD_PIXEL_CLOCK_HZ    (400 * 1000)
+#define EXAMPLE_PIN_NUM_SDA           3
+#define EXAMPLE_PIN_NUM_SCL           4
+#define EXAMPLE_PIN_NUM_RST           -1
+#define EXAMPLE_I2C_HW_ADDR           0x3C
+
+// The pixel number in horizontal and vertical
+#if CONFIG_EXAMPLE_LCD_CONTROLLER_SSD1306
+#define EXAMPLE_LCD_H_RES              128
+#define EXAMPLE_LCD_V_RES              64
+#elif CONFIG_EXAMPLE_LCD_CONTROLLER_SH1107
+#define EXAMPLE_LCD_H_RES              64
+#define EXAMPLE_LCD_V_RES              128
+#endif
+// Bit number used to represent command and parameter
+#define EXAMPLE_LCD_CMD_BITS           8
+#define EXAMPLE_LCD_PARAM_BITS         8
 
 /**
  * This is an example which echos any data it receives on configured UART back to the sender,
@@ -35,7 +71,7 @@
 #define ECHO_UART_BAUD_RATE     115200
 #define ECHO_TASK_STACK_SIZE    2048
 
-static const char *TAG = "UART TEST";
+static const char *TAG = "bulletin";
 
 #define BUF_SIZE (1024)
 
